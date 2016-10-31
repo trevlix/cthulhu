89c037d748236b7d6f57d033884639b9a7d968e0
trevlix <tyler@hudakville.com>
2016-10-31 10:26:40 -0400

    Adding setup.py install script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='CoC 7th Ed Combat',
+      version='0.10',
+      description='Call of Cthulhu 7th Ed Combat Simulation',
+      author='Tyler Hudak',
+      author_email='trevlix@gmail.com',
+      url='https://github.com/trevlix/cthulhu',
+      packages=['cthulhu'],
+     )
